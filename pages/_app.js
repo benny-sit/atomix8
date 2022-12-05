a17702ebbcd8e927bc5b109574dfe447ef218ab6
benny-sit <benysit1234@gmail.com>
2022-12-05 10:21:53 +0200

    try fonts 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import DefaultLayout from '../components/Default/DefaultLayout'
 import '../styles/globals.css'
+import '../public/fonts/fontsStyles.css'
 
 function MyApp({ Component, pageProps }) {
   return (
